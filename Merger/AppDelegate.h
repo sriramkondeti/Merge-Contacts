d74a4e55f34df9c66c7215935f402a690ef5bc58
Sri Ram <sriram_Kondeti@manulife.com>
2016-12-20 17:56:12 +0800

    Initial Commit

---
+++ Merger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Merger
+//
+//  Created by Sri Ram on 12/20/16.
+//  Copyright © 2016 Sri Ram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Merger/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Merger
+//
+//  Created by Sri Ram on 12/20/16.
+//  Copyright © 2016 Sri Ram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
